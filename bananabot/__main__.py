6ab38b574cb8369d9d123cec831140087c154e2e
SquishyStrawberry <SquishyStrawberry@users.noreply.github.com>
2016-07-30 22:16:43 +0200

    Add config parsing

--- bananabot/__main__.py
+++ bananabot/__main__.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 # flake8: noqa
+import argparse
+
+import bananabot
 
 
 def main():
-    pass
+    argp = argparse.ArgumentParser("bananabot")
+    argp.add_argument("configfile", nargs="?", default="config.yaml",
+                      help="The relative path to your config file.")
+    argv = argp.parse_args()
+    with open(argv.configfile) as config_fobj:
+        bananabot.BananaBot(yaml.load(config_fobj)).mainloop()
 
 if __name__ == "__main__":
     main()
