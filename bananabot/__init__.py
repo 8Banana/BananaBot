eb29966bce56c988dc11bbce0022c3defc49af25
SquishyStrawberry <SquishyStrawberry@users.noreply.github.com>
2016-07-31 03:40:21 +0200

    Add basic IRC functions.
    The bot now can connect with a given nickname and handle receiving messages,
    however so far it only handles responding to PINGs with PONGs.

--- bananabot/__init__.py
+++ bananabot/__init__.py
@@ -1,3 +1,4 @@
 """A module for a simple irc bot."""
+from .bananabot import BananaBot
 
 __version__ = "0.1.0"
--- bananabot/bananabot.py
+++ bananabot/bananabot.py
@@ -1,10 +1,44 @@
 #!/usr/bin/env python3
 """The base of the project, also includes some helper functions."""
+import collections
+import re
+import socket
 
 __all__ = ("BananaBot",)
 
 
 ACTION_FORMAT = "\x01ACTION {0}\x01"
+USER_REGEXP = re.compile(
+    r"^(?P<nick>.*?)!(?P<user>.*?)@(?P<host>.*?)$"
+)
+
+User = collections.namedtuple("User", ("nick", "username", "hostname"))
+Server = collections.namedtuple("Server", ("hostname",))
+ServerMessage = collections.namedtuple("ServerMessage",
+                                       ("sender", "command", "args"))
+
+
+def _parse_server_message(msg):
+    # Watch out, MSG is bad for you!
+    # ASSUMPTION: The message has the sender information.
+    sender_info, msg = msg[1:].split(" ", 1)
+    if "!" in sender_info:
+        match = USER_REGEXP.match(sender_info)
+        assert match is not None
+        sender = User(match.group("nick"),
+                      match.group("user"),
+                      match.group("host"))
+    else:
+        sender = Server(sender_info,)
+    args = msg.split(" ")
+    command = args.pop(0)
+    for n, i in enumerate(args[:]):
+        if i.startswith(":"):
+            temp = args[n:]
+            args = args[:n]
+            args.append(" ".join(temp)[1:])
+            break
+    return ServerMessage(sender, command, args)
 
 
 class BananaBot:
@@ -22,6 +56,15 @@ class BananaBot:
             config(dict): The configuration file for the bot.
         """
         self.config = config
+        self._socket = socket.socket()
+        self._linebuffer = collections.deque()
+
+    def _send(self, text):
+        if isinstance(text, str):
+            text = text.encode("utf-8")
+        if not text.endswith(b"\r\n"):
+            text += b"\r\n"
+        return self._socket.sendall(text)
 
     def send_privmsg(self, recipient, text):
         """Send a private message to a channel or an user."""
@@ -31,6 +74,27 @@ class BananaBot:
         """Send an action to a channel or an user."""
         return self.send_privmsg(recipient, ACTION_FORMAT.format(act))
 
+    def _recv_message(self):
+        if not self._linebuffer:
+            buf = b""
+            while not buf.endswith(b"\r\n"):
+                buf += self._socket.recv(4096)
+            rough = buf.decode("utf-8").split("\r\n")
+            self._linebuffer.extend(i for i in rough if i)
+        return _parse_server_message(self._linebuffer.popleft())
+
+    def _connect(self):
+        self._socket.connect((self.config["host"], self.config["port"]))
+
+    def _identify(self):
+        self._send("NICK {}".format(self.config["nickname"]))
+        self._send("USER {0} * * {0}".format(self.config["nickname"]))
+
     def mainloop(self):
         """Run the bot until the heat death of the universe."""
-        pass
+        self._connect()
+        self._identify()
+        while True:
+            msg = self._recv_message()
+            if msg.command == "PING":
+                self._send("PONG {}".format(" ".join(msg.args)))
