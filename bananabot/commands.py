6cc7460971efde02dc8cb076e736f9fa1ae09754
SquishyStrawberry <SquishyStrawberry@users.noreply.github.com>
2016-08-01 18:03:26 +0200

    Call handlers on PRIVMSG/JOIN/PART

---
+++ bananabot/commands.py
@@ -0,0 +1,10 @@
+"""This module is sort of a config file for all the command handlers."""
+
+__all__ = ("handlers",)
+
+handlers = {
+    "join": [],
+    "part": [],
+    "privmsg": [],
+    "command": {},
+}
